b055c8bbda1cd0733662e9e84d4f0ba3a8377311
rizwan230698 <rizwananwar230698@gmail.com>
2020-04-14 20:39:04 +0530

    Chat app socket.io

--- src/index.js
+++ src/index.js
@@ -91,6 +91,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log("server started");
 });
